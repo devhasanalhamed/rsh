89fa0d1c55adff56bac8e8e9cf400e3a7adf6cbf
Hasan Al-Hamed <therealhassan97@gmail.com>
2025-06-06 20:13:05 +0300

    Update app_constants.dart

--- lib/core/constants/app_constants.dart
+++ lib/core/constants/app_constants.dart
@@ -1,7 +1,7 @@
 class AppConstants {
   static const String appName = 'Timer App';
 
-  static const int maxTimerHours = 23;
-  static const int maxTimerMinutes = 59;
-  static const int maxTimerSeconds = 59;
+  static const int maxTimerHours = 24;
+  static const int maxTimerMinutes = 60;
+  static const int maxTimerSeconds = 60;
 }
