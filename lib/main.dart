a603cc6be68eea33cb30dd2643fe30f7769f00f3
Hasan Al-Hamed <therealhassan97@gmail.com>
2025-06-06 20:13:37 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:timer/core/services/notification_service.dart';
@@ -7,5 +8,8 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await NotificationService.initialize();
 
-  runApp(const ProviderScope(child: TimerApp()));
+  // runApp(const ProviderScope(child: TimerApp()));
+  runApp(
+    DevicePreview(builder: (context) => const ProviderScope(child: TimerApp())),
+  );
 }
