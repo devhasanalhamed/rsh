72840d5061c3d80e8a6d9222577858963fbe7608
Hasan Al-Hamed <therealhassan97@gmail.com>
2025-06-06 20:14:06 +0300

    Update timer_app.dart

--- lib/timer_app.dart
+++ lib/timer_app.dart
@@ -1,7 +1,8 @@
+import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:timer/core/themes/app_theme.dart';
-import 'package:timer/presentation/pages/timer_page.dart';
+import 'package:timer/presentation/screens/home_screen.dart';
 import 'package:timer/presentation/providers/theme_provider.dart';
 
 class TimerApp extends ConsumerWidget {
@@ -12,12 +13,14 @@ class TimerApp extends ConsumerWidget {
     final isDarkMode = ref.watch(themeProvider);
 
     return MaterialApp(
-      title: 'Timer',
+      title: 'RSH',
       theme: AppTheme.lightTheme,
       darkTheme: AppTheme.darkTheme,
       themeMode: isDarkMode ? ThemeMode.dark : ThemeMode.light,
-      home: const TimerPage(),
+      home: const HomeScreen(),
       debugShowCheckedModeBanner: false,
+      builder: DevicePreview.appBuilder,
+      locale: DevicePreview.locale(context),
     );
   }
 }
