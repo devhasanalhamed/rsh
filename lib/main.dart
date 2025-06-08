759e9f9d32fb2aba78726e9c2dcaacc66be8daa4
Hasan Al-Hamed <therealhassan97@gmail.com>
2025-06-08 17:03:46 +0300

    Update main.dart
    initialize vibration service

--- lib/main.dart
+++ lib/main.dart
@@ -2,14 +2,16 @@ import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:timer/core/services/notification_service.dart';
+import 'package:timer/core/services/vibration_service.dart';
 import 'package:timer/timer_app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await NotificationService.initialize();
+  await VibrationService.instance.initialize();
 
-  // runApp(const ProviderScope(child: TimerApp()));
-  runApp(
-    DevicePreview(builder: (context) => const ProviderScope(child: TimerApp())),
-  );
+  runApp(const ProviderScope(child: TimerApp()));
+  // runApp(
+  //   DevicePreview(builder: (context) => const ProviderScope(child: TimerApp())),
+  // );
 }
